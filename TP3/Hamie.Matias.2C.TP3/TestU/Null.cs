af97236a984198f427e2f01e74d1cac2e7cf0e8c
matiashamie <mnhamie@gmail.com>
2019-11-09 09:59:40 -0300

    asd

---
+++ TP3/Hamie.Matias.2C.TP3/TestU/Null.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EntidadesAbstractas;
+using EntidadesInstanciables;
+using Excepciones;
+
+namespace TestU
+{
+    [TestClass]
+    public class Null
+    {
+        [TestMethod]
+        public void TestNotNullListAlumnos()
+        { 
+            Universidad universidad = new Universidad();
+            Assert.IsNotNull(universidad.Alumnos);
+        }
+        [TestMethod]
+        public void TestNotNullListInstructores()
+        {
+            Universidad universidad = new Universidad();
+            Assert.IsNotNull(universidad.Instructores);
+        }
+        [TestMethod]
+        public void TestNotNullListJornadas()
+        {
+            Universidad universidad = new Universidad();
+            Assert.IsNotNull(universidad.Jornadas);
+        }
+    }
+}
