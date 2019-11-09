cc177be4aee1325368cfbfa86471087c43c78e28
matiashamie <mnhamie@gmail.com>
2019-11-09 09:56:28 -0300

    NacionalidadInvalidaTest

---
+++ TP3/Hamie.Matias.2C.TP3/TestU/NacionalidadInvalida.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EntidadesAbstractas;
+using EntidadesInstanciables;
+using Excepciones;
+
+namespace TestU
+{
+    [TestClass]
+    public class NacionalidadInvalida
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NacionalidadInvalidaException))]
+        public void TestMethod1()
+        {
+            Alumno alumno = new Alumno(3, "Matias", "Hamie", "35630323", Persona.ENacionalidad.Extranjero, Universidad.EClases.Programacion, Alumno.EEstadoCuenta.AlDia);
+        }
+    }
+}
