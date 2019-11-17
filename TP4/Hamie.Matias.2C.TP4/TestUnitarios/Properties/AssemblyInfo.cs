018f182189a4dbf9e6c068652ccc9b925750c5e7
matiashamie <mnhamie@gmail.com>
2019-11-17 13:58:40 -0300

    Casi terminado, solo falta punto 8 del pdf parte Formulario

---
+++ TP4/Hamie.Matias.2C.TP4/TestUnitarios/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnitarios")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestUnitarios")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("69a4e677-d2b5-45f0-9288-6b9c865b4359")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TP4/Hamie.Matias.2C.TP4/TestUnitarios/TestUnitariosCorreo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Entidades;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class TestUnitariosCorreo
+    {
+        [TestMethod]
+        public void PaqueteInstanciado()
+        {
+            Correo c = new Correo();
+
+            Assert.IsInstanceOfType(c.Paquetes, typeof(List<Paquete>));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TrackingIdRepetidoException))]
+        public void PaquetesIguales()
+        {
+            Correo c = new Correo();
+            Paquete p1 = new Paquete("sarasa 162", "0303456");
+            Paquete p2 = new Paquete("sin nombre 856", "0303456");
+
+            c += p1;
+            c += p2;
+        }
+    }
+}
